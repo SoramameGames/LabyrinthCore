91d81aa6f63e04999fc19ce2777e97298881e61c
Tomoya Ito <wanchan.mobile@gmail.com>
2019-10-06 21:12:28 +0900

    C++をプロジェクトから除外

--- Source/LabyrinthCore/Private/LabyrinthBlueprintLibrary.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "LabyrinthBlueprintLibrary.h"
-
-bool ULabyrinthBlueprintLibrary::IsInEditor()
-{
-	return GIsEditor;
-}
